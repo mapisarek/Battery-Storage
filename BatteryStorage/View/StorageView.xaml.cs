cb9b3b2e594b249c54c71e2541f4bad994e4aec2
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-16 21:47:46 +0200

    Added storageView constructor in codebehind

--- BatteryStorage/View/StorageView.xaml.cs
+++ BatteryStorage/View/StorageView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BatteryStorage.Model;
+using BatteryStorage.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,22 @@ namespace BatteryStorage.View
     /// </summary>
     public partial class StorageView : UserControl
     {
+        //Inicjalizacja viewmodelu z którego korzystamy w całym widoku
+        private StorageViewModel storageViewModel;
+
+        //Konstruktor widoku
         public StorageView()
         {
+            //Utworzenie nowego obiektu klasy StorageViewModel
+            storageViewModel = new StorageViewModel();
+            //Inicjalizacja zawartości okna
             InitializeComponent();
+            //Zbindowanie zawartości tabeli do widoku viewmodelu
+            this.DataGrid.ItemsSource = storageViewModel.ItemsView;
+            //Przypisanie filtru do metody UserFilter
+            storageViewModel.ItemsView.Filter = UserFilter;
         }
+
+
     }
 }
