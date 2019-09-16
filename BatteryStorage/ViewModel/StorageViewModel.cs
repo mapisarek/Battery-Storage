e5e6d38d50395fe91bb7d4becc5cc6aefe7c9a98
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-16 21:24:17 +0200

    Added Storage viewmodel with constructor and sort&find feature

---
+++ BatteryStorage/ViewModel/StorageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using BatteryStorage.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BatteryStorage.ViewModel
+{
+    class StorageViewModel
+    {
+        //Kolekcja baterii
+        public ObservableCollection<Battery> listOfBatteries;
+        //Rozszerzenie funkcjonalności kolekcji o sortowanie, filtrowanie rekordów
+        public ICollectionView ItemsView
+        {
+            get { return CollectionViewSource.GetDefaultView(listOfBatteries); }
+        }
+
+        //Konstruktor inicjujący nowa liste baterii i wpisujący przykładowe dane
+        public StorageViewModel()
+        {
+            listOfBatteries = new ObservableCollection<Battery>();
+            InitList();
+        }
+
+      
+    }
+}
