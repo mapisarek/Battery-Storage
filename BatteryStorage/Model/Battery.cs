1932d1eb35ad81f1b2c933018d32b5c937cc246f
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-16 21:22:12 +0200

    Added battery model with properties

---
+++ BatteryStorage/Model/Battery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatteryStorage.Model
+{
+    class Battery
+    {
+
+        public bool IsSelected { get; set; }
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public double Voltage { get; set; }
+        public int Capacity { get; set; }
+        public double Resistance { get; set; }
+        public int LevelOfCharge { get; set; }
+    }
+}
