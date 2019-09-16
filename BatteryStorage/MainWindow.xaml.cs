b72bcb5d93da56b3b6de40852db63562a4ed14c0
Marek Pisarek <marekpisarek94@gmail.com>
2019-09-16 21:29:49 +0200

    Added binding window content in code-behind

--- BatteryStorage/MainWindow.xaml.cs
+++ BatteryStorage/MainWindow.xaml.cs
@@ -20,9 +20,26 @@ namespace BatteryStorage
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Inicjalizacja widoków w oknie
+        StartView startView;
+        StorageView storageView;
+
         public MainWindow()
         {
+            //Utworzenie nowego obiektu widoku
+            startView = new StartView();
             InitializeComponent();
+            //Przypisanie zawartości okna
+            DataContext = startView;
+            //Zmiana zawartości okna na widok magazynu po wybraniu przycisku Start
+            this.startView.StartApp += new EventHandler(startAppView);
+        }
+
+        //Utworzenie nowego widoku i przypisanie zawartości okna do niego
+        public void startAppView(object sender, EventArgs e)
+        {
+            storageView = new StorageView();
+            DataContext = storageView;
         }
     }
 }
